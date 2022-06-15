e552a1d1081c7a7339c61dd44c011ffe0b922551
Vivek Kumar <www.vivekkumar0412@gmail.com>
2022-06-15 22:18:35 +0530

    onchange event

--- input.js
+++ input.js
@@ -1,1 +1,5 @@
-alert("check");
+const change = () =>{
+   let inp = document.getElementById('inp').value;
+   let icecream =  document.getElementById('icecream').value;
+   console.log(`${inp} selects ${icecream}`);
+}
